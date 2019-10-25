a95bd47c416de374f8085d8e29d5fde42a7751f1
Bin Liu <bliu@adobe.com>
2019-10-25 13:05:43 -0700

    Fix build error.

--- Demo/YYTextDemo/YYTextEditExample.m
+++ Demo/YYTextDemo/YYTextEditExample.m
@@ -38,13 +38,9 @@
     [self initImageView];
     __weak typeof(self) _self = self;
     
-    UIView *toolbar;
-    if ([UIVisualEffectView class]) {
-        toolbar = [[UIVisualEffectView alloc] initWithEffect:[UIBlurEffect effectWithStyle:UIBlurEffectStyleExtraLight]];
-    } else {
-        toolbar = [UIToolbar new];
-    }
-    toolbar.size = CGSizeMake(kScreenWidth, 40);
+    UIView *toolbar = [UIToolbar new];
+
+    toolbar.size = CGSizeMake(kScreenWidth, 60);
     toolbar.top = kiOS7Later ? 64 : 0;
     [self.view addSubview:toolbar];
     
